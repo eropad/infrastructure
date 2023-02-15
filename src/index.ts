bddd1572bb5a358c97dff71101b202b92cedb89b
alias-rahil <rahil.kabani.4@gmail.com>
2023-02-15 16:14:07 +0530

    refactor: minor code restructuring

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import path from 'node:path';
 import * as digitalocean from '@pulumi/digitalocean';
 import * as kubernetes from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
 
 const config = new pulumi.Config();
+const doConfig = new pulumi.Config('digitalocean');
 const minNodeCount = config.getNumber('minNodeCount') ?? 1;
 const maxNodeCount = config.getNumber('maxNodeCount') ?? 5;
 const region = config.getSecret('region') ?? 'region';
+const doCreds = doConfig.getSecret('token') ?? 'digitalocean:token';
+const githubUsername = config.getSecret('githubUsername') ?? 'githubUsername';
+const githubPassword = config.getSecret('githubPassword') ?? 'githubPassword';
+const argocdAdminPasswordBcryptHash = config.getSecret('argocdAdminPasswordBcryptHash') ?? 'argocdAdminPasswordBcryptHash';
+const domain = config.getSecret('domain') ?? 'domain';
+const repoUrl = config.getSecret('repoUrl') ?? 'repoUrl';
+const mailgunAuthPass = config.getSecret('mailgunAuthPass') ?? 'mailgunAuthPass';
+const doAccessKeyId = config.getSecret('doAccessKeyId') ?? 'doAccessKeyId';
+const doSecretAccessKey = config.getSecret('doSecretAccessKey') ?? 'doSecretAccessKey';
+const doBucket = config.getSecret('doBucket') ?? 'doBucket';
+const s3Endpoint = config.getSecret('s3Endpoint') ?? 's3Endpoint';
 
 const cluster = new digitalocean.KubernetesCluster('do-cluster', {
 	region,
@@ -30,8 +41,8 @@ const kubeconfig = cluster.status.apply(status => {
 	return cluster.kubeConfigs[0].rawConfig;
 });
 
-const provider = new kubernetes.Provider('do-k8s', {kubeconfig});
 const app = 'argocd';
+const provider = new kubernetes.Provider('do-k8s', {kubeconfig});
 
 const chartNamespace = new kubernetes.core.v1.Namespace(
 	app,
@@ -42,19 +53,6 @@ const chartNamespace = new kubernetes.core.v1.Namespace(
 		provider,
 	},
 );
-
-const doConfig = new pulumi.Config('digitalocean');
-const doCreds = doConfig.getSecret('token') ?? 'digitalocean:token';
-const githubUsername = config.getSecret('githubUsername') ?? 'githubUsername';
-const githubPassword = config.getSecret('githubPassword') ?? 'githubPassword';
-const argocdAdminPasswordBcryptHash = config.getSecret('argocdAdminPasswordBcryptHash') ?? 'argocdAdminPasswordBcryptHash';
-const domain = config.getSecret('domain') ?? 'domain';
-const repoUrl = config.getSecret('repoUrl') ?? 'repoUrl';
-const mailgunAuthPass = config.getSecret('mailgunAuthPass') ?? 'mailgunAuthPass';
-const doAccessKeyId = config.getSecret('doAccessKeyId') ?? 'doAccessKeyId';
-const doSecretAccessKey = config.getSecret('doSecretAccessKey') ?? 'doSecretAccessKey';
-const doBucket = config.getSecret('doBucket') ?? 'doBucket';
-const s3Endpoint = config.getSecret('s3Endpoint') ?? 's3Endpoint';
 
 const chartValues = {
 	githubUsername,
@@ -75,8 +73,7 @@ Reflect.construct(kubernetes.helm.v3.Chart, [
 	app,
 	{
 		namespace: chartNamespace.metadata.name,
-		// eslint-disable-next-line unicorn/prefer-module
-		path: path.join(__dirname, '..', app),
+		path: `../${app}`,
 		values: chartValues,
 	},
 	{
@@ -84,17 +81,15 @@ Reflect.construct(kubernetes.helm.v3.Chart, [
 	},
 ]);
 
-type Secret = {
-	metadata: {
-		name: string;
-	};
-	data?: Record<string, string | pulumi.Output<string>>;
-	stringData?: Record<string, string | pulumi.Output<string>>;
-};
-
 Reflect.construct(kubernetes.yaml.ConfigGroup, ['do-db-operator', {
 	files: 'https://raw.githubusercontent.com/digitalocean/do-operator/main/releases/do-operator-v0.1.5.yaml',
-	transformations: [((secret: Secret) => {
+	transformations: [((secret: {
+		metadata: {
+			name: string;
+		};
+		data?: Record<string, string | pulumi.Output<string>>;
+		stringData?: Record<string, string | pulumi.Output<string>>;
+	}) => {
 		if (secret.metadata.name === 'do-operator-do-api-token') {
 			delete secret.data;
 			secret.stringData = {'access-token': doCreds};
